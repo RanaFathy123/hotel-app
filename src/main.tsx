95dc1bf10a9e770ab961a29a92da2962cb4ca00e
RanaFathy123 <134057137+RanaFathy123@users.noreply.github.com>
2024-06-01 11:29:51 +0300

    uninstall bootstrap

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,5 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.js';
 import '@fortawesome/fontawesome-free';
 import "./index.css";
 import 'react-toastify/dist/ReactToastify.css';
