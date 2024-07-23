571a1c15cff6304370f983f30922bf820ef2f5fc
RanaFathy123 <134057137+RanaFathy123@users.noreply.github.com>
2024-07-23 20:33:35 +0300

    deploy issue

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,8 @@ import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  build: {
-    rollupOptions: {
-      external: ['@stripe/stripe-js'], // Ensure this module is not treated as external
-    },
-  },
-  resolve: {
-    alias: {
-      'prop-types': 'prop-types/index.js',
-    },
-  },
+  plugins: [react()],
+
   optimizeDeps: {
     include: ['@stripe/stripe-js']
   }
